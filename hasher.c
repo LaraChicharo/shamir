2b15d6aecd3ca0672b637ebe75f8a90fb197ab88
larachicharo <laralacesar@gmail.com>
2017-12-09 21:34:46 -0600

    Creating function to show the bit representation of a string.

--- hasher.c
+++ hasher.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
 #include <mhash.h>
 #include <string.h>
+#include <math.h>
+#include <assert.h>
 
+/**
+ *Takes the byte representation of a string and converts it to int.
+ *@param str: pointer to string.
+ *@param str_size: size of the string (including null byte).
+ *@param buff: store string binary representation here.
+ *It must be (((str_size - 1) * 8) + 1) long.
+ *@returns number.
+ */
+void string_as_binary(
+	char **str, unsigned int str_size, char **buff) {
+	
+	int i;
+	int j;
+	char res;
+	char k;
+	for (i = 0; i < str_size - 1; i++) {
+		k = (*str)[i];
+		for (j = 7; j >= 0 ; j--) {
+			if ((k >> j) & 1)
+				(*buff)[i*8 + 7-j] = '1';
+			else
+				(*buff)[i*8 + 7-j] = '0';
+			puts("noce");
+		}
+	}
+	*buff[str_size*8 + 1] = '\0';
+}
 
-int hash_string(char **str, char **buff) {
+void hash_string(char **str, char **buff) {
 	MHASH mh = mhash_init(MHASH_SHA256);
 	mhash(mh, *str, strlen(*str));
 	mhash_deinit(mh, *buff);
 }
 
 int main(int argc, char** argv) {
-	char *hash = malloc(sizeof(char) * 33);
-	hash_string(&argv[1], &hash);
-	printf(
-		"pass: %s, hash:%s, hashlen:%d\n",
-		argv[1], hash, strlen(hash));
-	free(hash);
+	int str_len = strlen(argv[1]);
+	char *buff = malloc((sizeof(char) * str_len * 8) + 1);
+	string_as_binary(
+		&argv[1], str_len + 1, &buff);
+	printf("binary repr: %s\n", buff);
+	free(buff);
 	return 0;
 }
+
