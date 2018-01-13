62309cc7183c348d5782e293b754d93069cd7a8c
larachicharo <laralacesar@gmail.com>
2018-01-13 16:04:23 -0600

    Custom name for newly created encrpyted file. Validating length of names of files specified in the input.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@
 #include <errno.h>
 #include "cipher.h"
 
+#define MAX_LEN_FILENAME 100  // Max len for filenames, including null
 #define MAX_PASS_LEN 3
+#define ENCRYPTED_EXT ".aes"
 
 
 void get_password(char **pass, int *keysize) {
@@ -78,6 +80,9 @@ void validate_file(char* filename, char* mode, char* errmsg) {
 void validate_cipher_option(int argc, char** argv) {
 	if (argc != 6)
 		error_message(1, "Not enough arguments!");
+	if (!(strlen(argv[1]) < MAX_LEN_FILENAME &&
+		strlen(argv[5]) < MAX_LEN_FILENAME))
+		error_message(1, "Filename too long.");
 	int nshares = str_to_int(argv[3]);
 	if (nshares <= 2)
 		error_message(1, "Too few shares!");
@@ -87,25 +92,61 @@ void validate_cipher_option(int argc, char** argv) {
 	validate_file(argv[5], "r", "Error reading file to encrypt");
 }
 
+/**
+ * Gets the length of the name of the encrypted file.
+ * Adds the name of the file to be encrypted and the extension
+ * of the new file.
+ * @param original_name name of the file to encrypt.
+ * @returns the length of the name of the new encrypted file.
+ * */
+int get_namesize_encrypted_file(char* original_name) {
+	return strlen(original_name) + strlen(ENCRYPTED_EXT) + 1;
+}
+
+/**
+ * Merges the name of the file to encrpt and a file extension.
+ * @param original name name of the file to encrypt.
+ * @param encname array of chars long enough to store the name
+ * of the new encrypted file.
+ */
+void get_name_encrypted_file(char* original_name, char** encname) {
+	char newname_[get_namesize_encrypted_file(original_name)];
+	
+	int i = 0;
+	while (original_name[i] != '\0') {
+		newname_[i] = original_name[i];
+		i++;
+	}
+	int j = 0;
+	int len = strlen(ENCRYPTED_EXT);
+	while (j < len)
+		newname_[i+j] = ENCRYPTED_EXT[j++];
+	newname_[i + j] = '\0';
+	strcpy(*encname, newname_);
+}
+
 int main(int argc, char** argv) {
 	if (argc < 4)
 		error_message(1, "Too few arguments");
 	if (strcmp(argv[1], "c") == 0) {
 				
 		validate_cipher_option(argc, argv);
-
 		char *pass = (char*)malloc(MAX_PASS_LEN * sizeof(char));
 		int keysize;
 
 		get_password(&pass, &keysize);
 
 		FILE *plainfp = fopen(argv[5], "r");
-		FILE *encrfp = fopen("encrypted.aes", "w");
+		char* encname = malloc(
+			sizeof(char) * get_namesize_encrypted_file(argv[5]));
+		get_name_encrypted_file(argv[5], &encname);
+		FILE *encrfp = fopen(encname, "w");
 		
 		encrypt(&plainfp, &encrfp, pass, keysize);
 
 		printf("Your pass: %s\n", pass);
 		free(pass);
+		free(encname);
 		fclose(plainfp);
 		fclose(encrfp);
 	} else if (strcmp(argv[1], "d") == 0) {
